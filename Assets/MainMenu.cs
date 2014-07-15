9848997197487c8634fd04b48cc69a46e71602cb
delorenj <jaradd@gmail.com>
2014-07-15 17:14:54 -0400

    Trying to cleanup some asset stuff

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+	Rect windowRect;
+	
+	// Use this for initialization
+	void Start () {
+		windowRect = new Rect(0, 0, Screen.width, Screen.height);
+	}
+	
+	void OnGui() {
+		GUI.Box(windowRect, "Main Menu");
+		GUI.Button(new Rect(370, 350, Screen.width/4, Screen.height/24), "Suck it");
+	}
+}
