b179f1265d39ace7dbc1a78097e95dfae6247636
delorenj <jaradd@gmail.com>
2014-07-15 23:07:02 -0400

    Reorganized project structure. Added title screen test. Added music loop test

---
+++ Assets/Scripts/TitleGUI.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleGUI : MonoBehaviour {
+
+	public GUISkin skin;
+	
+	int buttonHeight = 50;
+	int buttonWidth = 150;
+	float halfScreenWidth;
+	float halfButtonWidth;
+	
+	void Start() {
+		halfScreenWidth = Screen.width/2;
+		halfButtonWidth = buttonWidth/2;
+	}
+	
+	void OnGUI() {
+		GUI.skin = skin;
+		
+		if(GUI.Button(new Rect(halfScreenWidth-halfButtonWidth,800,buttonWidth,buttonHeight), "Start the Shit")) {
+			Application.LoadLevel("Forest");
+		}
+	}
+}
