229fe17ac4ad688068071880dfa4b2fe2d562b8a
delorenj <jaradd@gmail.com>
2014-08-08 20:49:33 -0400

    Added state machine base. Added GameState state machine to GameManager. Added Idle state and PlacingObject state

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,8 @@
 
 public class GameManager : Singleton<GameManager> {
 
+	public GameState stateMachine;
+	
 	public static void PlayIntro() {
 		SoundManager.PlaySong("Orbit01");
 	}
---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameState : StateMachineBase {
+	
+	public Transform objectPrefab;
+	
+	public enum GameStates
+	{
+		Idle = 0,
+		PlacingObject = 1
+	}
+	
+	
+	bool _busy;
+	
+	// Use this for initialization
+	void Start () {
+		currentState = GameStates.Idle;
+	}
+	
+	bool hasPlacedObject;
+	
+	#region Idle
+	
+	void Idle_EnterState()
+	{
+		print ("Idle_EnterState...");	
+	}
+	
+	void Idle_Update()
+	{
+		print ("Idle_Update()");
+	}
+	
+	#endregion	
+	
+	#region PlacedObject
+	
+	void PlacingObject_EnterState()
+	{
+		print ("PlacingObject_EnterState()");
+	}
+	
+	void PlacingObject_Update()
+	{
+		print ("PlacingObject_Update()");
+	}
+	
+	#endregion
+}
